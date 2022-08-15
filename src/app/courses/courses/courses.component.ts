783203ff852d7a756ee594def9005e56d14f09ec
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-08-15 14:43:41 -0300

    adicionando Observable no componente courses e chamando json

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 
@@ -8,9 +9,10 @@ import { CoursesService } from '../services/courses.service';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable <Course[]>;
   displayedColumns = ['name','categoria'];
   
   //coursesService: CoursesService;
