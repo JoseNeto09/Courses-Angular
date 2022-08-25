3c2b16e25bff75ade1f2d0537d91d815b84b33c7
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-08-25 11:32:15 -0300

    Salvando dados do formulario Cursos com http Post e tratando erros com MatSnackBarModule

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from '../services/courses.service';
+
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
@@ -10,7 +14,9 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar) { 
     this.form = this.formBuilder.group({
         name: [null],
         categoria: [null]
@@ -18,14 +24,19 @@ export class CourseFormComponent implements OnInit {
     }
 
   ngOnInit(): void {
-
   }
+  
   onSubmit(){
-
+    this.service.save(this.form.value)
+      .subscribe(result => console.log(result), error => this.onError());
   }
+
   onCancel(){
     
   }
 
+  private onError(){
+    this._snackBar.open('Error ao salvar curso.', ' ',  {duration: 5000});
+  }
 
 }
--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -9,6 +9,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+
 
 
 @NgModule({
@@ -22,7 +26,9 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatFormFieldModule,
     MatAutocompleteModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule
   ],
 })
 export class AppMaterialModule { }
