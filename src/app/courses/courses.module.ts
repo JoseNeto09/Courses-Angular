dbf77bff59b5b86397c5223ee6c863d2c24435fe
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-08-29 11:03:49 -0300

    Gerando component courses-list

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -8,12 +8,14 @@ import { AppMaterialModule } from '../shared/app-material/app-material.module'
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CourseFormComponent } from './course-form/course-form.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
 
 
 @NgModule({
   declarations: [
     CoursesComponent,
-    CourseFormComponent
+    CourseFormComponent,
+    CoursesListComponent
   ],
   imports: [
     CommonModule,
